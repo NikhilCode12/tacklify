3180b49d3e5432348939b841cd567006aacd21cf
Nikhil Sharma <promaxchessace@gmail.com>
2023-10-05 01:10:55 +0530

    add worldTimeAPI

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import axios from 'axios';
 const app = express();
 const port = 3000;
 app.use(express.static('public'));
@@ -17,30 +18,44 @@ const task2 = new TodoList( {name : 'Click the + button to add a new task in the
 const task3 = new TodoList( {name : 'Check the checkbox to mark a task completed.'} );
 
 const defaultTasks = [task1,task2,task3];
-let newTask = "";
+const worldTimeAPIURL = 'http://worldtimeapi.org/api/ip';
 
 app.get('/',async (req,res)=>{
     try{
+        const localTime = await axios.get(worldTimeAPIURL);
+        const currentTime = localTime.data.datetime.slice(11,19) + `${localTime.data.datetime.slice(11,13) < 12 ? ' AM' : ' PM'}`;
         const tasks = await TodoList.find({});
         if(tasks.length === 0){
             TodoList.insertMany(defaultTasks);
+            res.redirect('/');
         }
-        res.render('index.ejs',{
-            date_time : 'today',
-            item : newTask,
-            items : tasks,
-        });
+        else{
+            res.render('index.ejs',{
+                time: {
+                    dd: localTime.data.datetime.slice(8,10),
+                    mm: months[parseInt(localTime.data.datetime.slice(5,7)) - 1],
+                    yyyy: localTime.data.datetime.slice(0,4),
+                    time: currentTime
+                },
+                items : tasks,
+            });
+        }
     } catch(err){
         console.log(err);
     }
 });
 
 app.post('/',(req,res)=>{
-    newTask = req.body.task;
-    defaultTasks.push(newTask);
+    const taskName = req.body.task;
+    const newTask = new TodoList({ name: taskName });
+    newTask.save();
     res.redirect('/');
 })
 
 app.listen(port,()=>{
     console.log(`Listening on port : ${port}`);
 })
+
+const months = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug","Sept", "Oct", "Nov", "Dec"
+];
