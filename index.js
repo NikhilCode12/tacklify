3d8aa0b5da9caa999a9d158b3d8e9013c6564924
Nikhil Sharma <promaxchessace@gmail.com>
2023-10-05 03:29:05 +0530

    add mongodb to each route

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import axios from 'axios';
 const app = express();
 const port = 3000;
+const worldTimeAPIURL = 'http://worldtimeapi.org/api/ip';
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended : true}));
 
@@ -18,7 +19,13 @@ const task2 = new TodoList( {name : 'Click the + button to add a new task in the
 const task3 = new TodoList( {name : 'Check the checkbox to mark a task completed.'} );
 
 const defaultTasks = [task1,task2,task3];
-const worldTimeAPIURL = 'http://worldtimeapi.org/api/ip';
+
+const customTodoSchema = {
+    name : String,
+    xitems : [todoSchema] 
+}
+
+const CustomTodoList = mongoose.model('customList',customTodoSchema);
 
 app.get('/',async (req,res)=>{
     try{
@@ -31,6 +38,7 @@ app.get('/',async (req,res)=>{
         }
         else{
             res.render('index.ejs',{
+                title : 'Welcome',
                 time: {
                     dd: localTime.data.datetime.slice(8,10),
                     mm: months[parseInt(localTime.data.datetime.slice(5,7)) - 1],
@@ -45,12 +53,59 @@ app.get('/',async (req,res)=>{
     }
 });
 
-app.post('/',(req,res)=>{
+app.get('/:customListName',async (req,res)=>{
+    try{
+        const localTime = await axios.get(worldTimeAPIURL);
+        const currentTime = localTime.data.datetime.slice(11,19) + `${localTime.data.datetime.slice(11,13) < 12 ? ' AM' : ' PM'}`;
+        const customListName = req.params.customListName;
+        const cList = await CustomTodoList.findOne({name : customListName});
+
+        if(!cList){
+            const list = new CustomTodoList({
+                name : customListName,
+                xitems : defaultTasks
+            });
+            list.save();
+            res.redirect('/' + customListName);
+        }
+        else{
+            res.render('index.ejs',{
+                title : cList.name,
+                time: {
+                    dd: localTime.data.datetime.slice(8,10),
+                    mm: months[parseInt(localTime.data.datetime.slice(5,7)) - 1],
+                    yyyy: localTime.data.datetime.slice(0,4),
+                    time: currentTime
+                },
+                items: cList.xitems,
+            });
+        }
+    }catch(err){
+        console.log(err);
+    }
+});
+
+app.post('/', async (req,res)=>{
     const taskName = req.body.task;
+    const listName = req.body.list;
     const newTask = new TodoList({ name: taskName });
-    newTask.save();
-    res.redirect('/');
+
+    if(listName === 'Welcome'){
+        newTask.save();
+        res.redirect('/');
+    } else{
+        const newItem  = await CustomTodoList.findOne({name : listName});
+        newItem.xitems.push(newTask);
+        newItem.save();
+        res.redirect('/'+ listName);
+    }
 })
+
+app.post('/remove',async (req,res)=>{
+    const checkedTaskId = req.body.checkbox;
+    await TodoList.findByIdAndRemove({_id : checkedTaskId});
+    res.redirect('/');
+});
 
 app.listen(port,()=>{
     console.log(`Listening on port : ${port}`);
